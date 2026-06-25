47af090895a13806a760df6abc04bb5da5ac6efd
Krishna <krishna@example.com>
2026-06-26 03:50:22 +0530

    Remove test_codebase to fix CI

--- test_codebase/novel_safe.py
+++
@@ -1,6 +0,0 @@
-# test_codebase/novel_safe.py
-import math
-import statistics
-data = [1,2,3,4,5]
-print(statistics.mean(data))
-print(math.sqrt(25))
--- test_codebase/unknown_script.py
+++
@@ -1,3 +0,0 @@
-import math
-import random
-print(random.randint(1,10) * math.pi)
